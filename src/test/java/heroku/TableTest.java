2247827e6ab0508d01349f27b68c6a23c9693001
Doti Nguyen <doti0501@gmail.com>
2025-05-10 10:49:03 +0700

    Update TableTest file exercise Day 7

---
+++ src/test/java/heroku/TableTest.java
@@ -0,0 +1,37 @@
+package heroku;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class TableTest {
+    @Test
+    void tc05(){
+   WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/tables");
+        List<Double> dueList = driver
+                .findElements(By.xpath("//table[@id='table1']//tbody/tr/td[4]"))
+                .stream()
+                .map(cell -> Double.valueOf(cell.getText().replace("$", "")))
+                .collect(Collectors.toList());
+        double maxDue = Collections.max(dueList);
+int rowIndex = dueList.indexOf(maxDue) + 1;
+String lastname = driver.findElement(By.xpath(String.format("//table[@id='table1']//tbody/tr[%d]/td[1]", rowIndex)))
+        .getText();
+        String firstname = driver.findElement(By.xpath(String.format("//table[@id='table1']//tbody/tr[%d]/td[2]", rowIndex)))
+                .getText();
+        Assert.assertEquals(String.format("%s, %s", lastname, firstname), "Doe, Jason");
+        driver.close();
+    }
+
+
+}
+
